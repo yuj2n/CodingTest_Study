fcb517114eb141c77bdf64bde4360d5d7db27914
Baek, Sehee <tpgmlqor@gmail.com>
2024-01-10 23:00:50 +0900

    11021-A+B - 7
    A+B를 조금 더 아름답게 출력하는 문제

---
+++ beginner/week2/baeksehee/step3_11021.js
@@ -0,0 +1,29 @@
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 
+테스트 케이스 번호는 1부터 시작한다.
+*/
+
+const fs = require("fs");
+const input = fs
+  .readFileSync("./txt/step3_11021.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const num = parseInt(input[0]);
+
+for (let i = 1; i < num + 1; i++) {
+  const testCase = input[i].split(" ").map(Number);
+  const A = testCase[0];
+  const B = testCase[1];
+
+  console.log(`Case #${i}: ${A + B}`);
+}
