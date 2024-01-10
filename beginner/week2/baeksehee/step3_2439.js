a09ba705db0f15e4e25de8687da982cf658516f9
Baek, Sehee <tpgmlqor@gmail.com>
2024-01-10 23:11:33 +0900

    2439- 별 찍기 - 2
    별을 찍는 문제 2

---
+++ beginner/week2/baeksehee/step3_2439.js
@@ -0,0 +1,27 @@
+/*
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+*/
+
+const fs = require("fs");
+const input = fs.readFileSync("./txt/step3_2439.txt").toString().trim();
+
+const num = parseInt(input);
+
+for (let i = 1; i <= num; i++) {
+  let result = "";
+  for (let k = 1; k <= num - i; k++) {
+    result += " ";
+  }
+  for (let j = 1; j <= i; j++) {
+    result += "*";
+  }
+  console.log(result);
+}
