b44394317042e3f894c923909d9efdbacf74d787
전유진 <dbwls4595@naver.com>
2024-02-17 23:17:23 +0900

    10798-세로읽기
    문자열의 배열을 다루는 문제

---
+++ beginner/week6/week6_10798.js
@@ -0,0 +1,11 @@
+// 문자열의 배열을 다루는 문제
+const words = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const maxLength = Math.max(...words.map(i => i.length));
+let vertical = "";
+for (let i=0; i<maxLength; i++) {
+    for (let j=0; j<words.length; j++) {
+        vertical += words[j][i] || "";
+    }
+}
+
+console.log(vertical);
